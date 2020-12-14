cd8819a81c1c2e65178f1483e21fc1ddb422e0a7
Ben Oluwa <benfalowo@gmail.com>
2020-12-14 11:51:03 -0600

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ class TestHello(unittest.TestCase):
         
     def test_temp10(self):
         val = self.app.get('temps')
-        self.assertEqual(temp10(val), '10')
+        self.assertEqual(val.count(), '10')
 
     def test_recent_temps(self):
         tv = self.app.get('/recent_temps')
